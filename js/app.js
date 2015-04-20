7e93cacbff8b98cda7e2cad1f11fc37246aff6db
David Gomez <david.ernesto.gomez@correounivalle.edu.co>
2015-04-19 20:56:58 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+var sancochoApp = angular.module("sancochoApp", ['ngRoute']);
+
+
+sancochoApp.controller('sancochoCtrl',function(){});
+
+sancochoApp.controller('programacionCtrl', function ($scope, $http) {
+  $http.get('/datos/programacion.json').success(function(data) {
+    $scope.programacion = data;
+    console.log($scope.programacion);
+  });
+
+  
+});    
+
+
+
+sancochoApp.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                    when('/fest/', {
+                        templateUrl: '/html/festival.html',
+                        controller: 'sancochoCtrl'
+                    }).
+                    when('/programacion/', {
+                        templateUrl: '/html/programacion.html',
+                        controller: 'programacionCtrl'
+                    }).
+                    when('/pasados/', {
+                        templateUrl: '/html/pasados.html',
+                        controller: 'sancochoCtrl'
+                    }).
+                    when('/organiza/', {
+                        templateUrl: '/html/organizadores.html',
+                        controller: 'sancochoCtrl'
+                    }).
+                    when('/patrocinadores/', {
+                        templateUrl: '/html/patrocinadores.html',
+                        controller: 'sancochoCtrl'
+                    }).
+                    when('/contacto/', {
+                        templateUrl: '/html/contacto.html',
+                        controller: 'sancochoCtrl'
+                    }).
+                    when('/bienvenidos/', {
+                        templateUrl: '/html/bienvenidos.html',
+                        controller: 'sancochoCtrl'
+                    }).
+                    
+            
+            
+                    otherwise({
+                        redirectTo: '/'
+                    });
+        }]);
